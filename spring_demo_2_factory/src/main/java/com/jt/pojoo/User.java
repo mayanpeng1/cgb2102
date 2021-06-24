fbc674e7f9cc26691fd43386698be613267e19b6
马彦鹏 <1716098468@qq.com>
2021-06-24 16:19:33 +0800

    第一次上传

--- spring_demo_2_factory/src/main/java/com/jt/pojoo/User.java
+++ spring_demo_2_factory/src/main/java/com/jt/pojoo/User.java
@@ -1,12 +1,28 @@
 package com.jt.pojoo;
 
 public class User {
+    private String conn; //数据库连接
+
+    //1.实例化对象
     public User(){
         System.out.println("创建对象");
     }
 
-    public void say(){
+    //2.初始化方法
+    public void init(){
+        this.conn = "赋值数据源链接";
+        System.out.println(this.conn);
+    }
 
+    //3.用户调用的方法
+    public void say(){
         System.out.println("我是用户！！！");
     }
+
+    //4.销毁方法
+    public void destory(){
+        this.conn=null;
+        System.out.println("释放资源"+this.conn+"----");
+    }
+
 }
--- spring_demo_2_factory/src/test/java/com/jt/TestSpring.java
+++ spring_demo_2_factory/src/test/java/com/jt/TestSpring.java
@@ -3,6 +3,7 @@ package com.jt;
 import com.jt.pojoo.User;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Calendar;
@@ -10,36 +11,36 @@ import java.util.Calendar;
 public class TestSpring {
 
     @Test
-    public void testStatic(){
+    public void testStatic() {
         ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
         Calendar calendar1 = (Calendar) context.getBean("calendar1");
-        System.out.println("当前时间为："+calendar1.getTime());
+        System.out.println("当前时间为：" + calendar1.getTime());
     }
 
     @Test
-    public void testInstance(){
+    public void testInstance() {
         ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
         Calendar calendar2 = (Calendar) context.getBean("calendar2");
-        System.out.println("当前时间为："+calendar2.getTime());
+        System.out.println("当前时间为：" + calendar2.getTime());
 
     }
 
     @Test
-    public void testSpringFactory(){
+    public void testSpringFactory() {
         ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
         Calendar calendar3 = (Calendar) context.getBean("calendar3");
-        System.out.println("当前时间为："+calendar3.getTime());
+        System.out.println("当前时间为：" + calendar3.getTime());
 
     }
 
     @Test
-    public void testUser(){
+    public void testUser() {
         ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
-         context.getBean("user");
-         context.getBean("user");
-         context.getBean("user");
-         context.getBean("user");
-
-
+        context.getBean("user");
+        context.getBean("user");
+        context.getBean("user");
+        context.getBean("user");
     }
+
+
 }
