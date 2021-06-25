c4302a8dc912753fc897cfae9e272f2620cc3925
马彦鹏 <1716098468@qq.com>
2021-06-25 10:51:52 +0800

    第一次上传

--- spring_demo_5_annotation/src/main/java/com/jt/dao/UserDaoImpl.java
+++ spring_demo_5_annotation/src/main/java/com/jt/dao/UserDaoImpl.java
@@ -3,7 +3,11 @@ package com.jt.dao;
 import com.jt.pojo.User;
 import org.springframework.stereotype.Repository;
 
-@Repository
+/**
+ * <bean id="类名首字母小写~~userDaoImpl class=""UserDaoImpl.class">
+ * 如果需要修改beanId 则手动添加value属性即可
+ */
+@Repository(value = "userDao")
 public class UserDaoImpl implements UserDao{
 
     @Override
--- spring_demo_5_annotation/src/test/java/com/jt/TestSpring.java
+++ spring_demo_5_annotation/src/test/java/com/jt/TestSpring.java
@@ -16,8 +16,20 @@ public class TestSpring {
         System.out.println("恭喜学会MVC结构");
     }
 
+    //不好用
     @Test
     public void testSpring(){
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("application-annotation.xml");
+        UserController userController = (UserController) context.getBean("userController");
+        userController.addUser();
+        System.out.println(userController);
+    }
+
+
+
+    @Test
+    public void testController(){
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("application-annotation.xml");
         UserController userController = (UserController) context.getBean("userController");
