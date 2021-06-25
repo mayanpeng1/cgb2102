e4f66b1d4c556071a1cb138468a780002e4a5134
马彦鹏 <1716098468@qq.com>
2021-06-25 17:51:16 +0800

    第一次上传

--- spring_demo_6_mvc/src/test/java/com/jt/TestSpring.java
+++ spring_demo_6_mvc/src/test/java/com/jt/TestSpring.java
@@ -1,6 +1,7 @@
 package com.jt;
 
 import com.jt.controller.UserController;
+import com.jt.pojo.User;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -10,7 +11,8 @@ public class TestSpring {
     @Test
     public void test01(){
         ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
-        UserController userController = context.getBean(UserController.class);
+        // UserController userController = context.getBean(UserController.class);
+        UserController userController = (UserController) context.getBean("userController");
         userController.addUser();
     }
 }
