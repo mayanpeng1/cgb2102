720d16e0845a42c83c6819c36005647dff5b17e4
马彦鹏 <1716098468@qq.com>
2021-06-28 17:37:48 +0800

    第一次上传

--- springboot_demo_1/src/main/java/com/jt/pojo/Dept.java
+++ springboot_demo_1/src/main/java/com/jt/pojo/Dept.java
@@ -1,9 +1,19 @@
 package com.jt.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component  //将对象交给Spring容器管理
+@PropertySource(value = "classpath:/dept.properties",encoding = "UTF-8")
+@Data //data 是lombok使用最多的   自动生成get/set/toString/equals/hashcode 等方法
+@Accessors(chain = true) //重写set方法,返回this对象
+@NoArgsConstructor //无参构造
+@AllArgsConstructor //全参构造
 public class Dept {
     //spel表达式
     @Value("${dept.id}")
@@ -11,27 +21,19 @@ public class Dept {
     @Value("${dept.name}")
     private String name;
 
+    @Value("${dept.id2}")
+    private Integer id2;
+
+    @Value("${dept.name2}")
+    private String name2;
+
     @Override
     public String toString() {
         return "Dept{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", id2=" + id2 +
+                ", name2='" + name2 + '\'' +
                 '}';
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
