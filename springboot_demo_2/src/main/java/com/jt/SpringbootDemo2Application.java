b649122a8770adb7192b2f62b07cbafb7e647e9c
马彦鹏 <1716098468@qq.com>
2021-06-29 18:01:58 +0800

    第一次上传

--- springboot_demo_2/src/main/java/com/jt/SpringbootDemo2Application.java
+++ springboot_demo_2/src/main/java/com/jt/SpringbootDemo2Application.java
@@ -1,9 +1,12 @@
 package com.jt;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//mapper 接口注解的包扫描  以后不需要再次编辑单个@Mapper注解
+@MapperScan("com.jt.mapper")
 public class SpringbootDemo2Application {
 
 	public static void main(String[] args) {
--- springboot_demo_2/src/test/java/com/jt/SpringbootDemo2ApplicationTests.java
+++ springboot_demo_2/src/test/java/com/jt/SpringbootDemo2ApplicationTests.java
@@ -1,13 +1,23 @@
 package com.jt;
 
+import com.jt.mapper.UserMapper;
+import com.jt.pojo.DemoUser;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.SQLOutput;
+import java.util.List;
 
 @SpringBootTest
 class SpringbootDemo2ApplicationTests {
 
+	@Autowired
+	private UserMapper userMapper;
 	@Test
 	void contextLoads() {
+		List<DemoUser> list = userMapper.findAll();
+		System.out.println(list);
 	}
 
 }
