3f23eeb8545744b7f201423d7185c6558def050d
马彦鹏 <1716098468@qq.com>
2021-07-01 11:25:51 +0800

    第一次上传

---
+++ springboot_demo_3_mvc/src/main/java/com/jt/SpringbootDemo3MvcApplication.java
@@ -0,0 +1,13 @@
+package com.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemo3MvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemo3MvcApplication.class, args);
+	}
+
+}
