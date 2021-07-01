8a3040055db53223c3ddc62d8b0ed5714c12cfdf
马彦鹏 <1716098468@qq.com>
2021-07-01 11:57:57 +0800

    第一次上传

---
+++ springboot_demo_3_mvc/src/main/java/com/jt/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.jt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller// 1.将该类交由Spring容器管理   2.同时开启SpringMVC机制
+public class HelloController {
+    /**
+     * 需求: http://localhost:8090/hello   访问hello.html
+     * 实现步骤:
+     *      1.拦截用户请求 @RequestMapping("/hello")
+     *      2.String 类型的返回值 表示返回页面名称
+     *      3.根据YML配置文件中的内容 动态的拼接前缀和后缀 形成页面唯一路径
+     */
+
+    @RequestMapping("/hello")
+    public String hello(){
+        //动态的拼接前缀+后缀
+        //classpath:/templates/hello.html
+        return "hello";
+    }
+}
