21367d944d266c994aef73f8b9f909721054d7bf
马彦鹏 <1716098468@qq.com>
2021-07-03 00:40:16 +0800

    第一次上传

--- springboot_demo_3_mvc/src/main/java/com/jt/controller/JSONController.java
+++ springboot_demo_3_mvc/src/main/java/com/jt/controller/JSONController.java
@@ -1,11 +1,10 @@
 package com.jt.controller;
 
 import com.jt.pojo.User;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController //@Controller+@ResponseBody 该类中所有的方法的返回值都是JSON串
 public class JSONController {
     /**
      * 需求: 要求根据getJSON的请求,获取User对象的JSON数据
@@ -23,7 +22,7 @@ public class JSONController {
      * @return
      */
     @RequestMapping("/getJSON")
-    @ResponseBody
+    // @ResponseBody  返回值是一个JSON串
     public User getJSON(){
         User user = new User();
         user.setId(1).setName("JSON测试");
