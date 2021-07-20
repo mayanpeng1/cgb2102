8d018f5268139f012b4e31344136cc16c7fc3aaa
马彦鹏 <1716098468@qq.com>
2021-07-20 23:56:56 +0800

    jt项目

--- jtadmin/src/router/index.js
+++ jtadmin/src/router/index.js
@@ -5,13 +5,19 @@ import VueRouter from 'vue-router'
 //手动导入自己的组件
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import User from '../components/user/user.vue'
+import Welcome from '../components/Welcome.vue'
 
 //在vue对象中声明路由对象 整个脚手架 中都可以引用路由
 Vue.use(VueRouter)
 const routes = [
   {path: '/', redirect: '/login'},
   {path: '/login', component: Login},
-  {path:'/home', component: Home}
+  {path:'/home', component: Home, redirect:'/welcome',
+    children:[
+      {path:'/user',component:User},
+      {path:'/welcome', component:Welcome}
+  ]}
 ]
 
 //创建路由对象
